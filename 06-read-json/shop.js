649207ab78b75961f17ceddcdeaca975d64a93a8
kunxin-chor <chorkunxin@yahoo.com>
2022-02-09 09:16:20 +0000

    Added shop example

--- 06-read-json/shop.js
+++ 06-read-json/shop.js
@@ -1,17 +1,44 @@
 // METHOD 1
-async function main() {
-    let response = await axios.get('products.json');
-    for (let product of response.data) {
-        let optionElement = document.createElement('option');
-        optionElement.value = product.sku;
-        optionElement.innerHTML = product.name;
+// use a global variable to remember all the products
+let catalog = [];
 
-        document.querySelector('#product')
-                .appendChild(optionElement);
+async function main() {
+    try {
+        let response = await axios.get('products.json');
+        catalog = response.data;
+        for (let product of response.data) {
+            let optionElement = document.createElement('option');
+            optionElement.value = product.sku;
+            optionElement.innerHTML = product.name;
+    
+            document.querySelector('#product')
+                    .appendChild(optionElement);
+        }
+    }
+    catch(e) {
+        console.log("Unable to read json file");
     }
 }
 
 main();
+
+document.querySelector('#btnDisplay')
+        .addEventListener('click', function(){
+            let selectedProductSKU = document.querySelector('#product').value;
+            // catalog is a global variable that stores an array
+            // of all the products
+            let targetProduct = null;
+            for (let product of catalog) {
+                if (selectedProductSKU == product.sku) {
+                    targetProduct = product;
+                    break;
+                }
+            }
+            document.querySelector('#details').style.display = 'block';
+            document.querySelector('#name').innerHTML = targetProduct.name;
+            document.querySelector("#price").innerHTML = targetProduct.price;
+
+        })
 
 
 
