ac0027963c84d3a354df3dabd1bae0fe899e24fb
kunxin-chor <chorkunxin@yahoo.com>
2022-02-09 02:54:09 +0000

    Linear algo

--- 04-forms/survey.js
+++ 04-forms/survey.js
@@ -6,6 +6,19 @@ let btn = document.querySelector('#btn');
 btn.addEventListener('click', function(){
     let name = document.querySelector('#name').value;
     let email = document.querySelector('#email').value;
+    // let rating = document.querySelector('.rating:checked').value;
+    let rating = null; // before we run the for-loop, we don't
+                       // know which one has been selected
 
-    console.log(name, email);
+    // get all the radio buttons with .rating in one array
+    let rb = document.querySelectorAll('.rating');
+    for (let eachRating of rb) {
+        // for each rating radio button, check if its .checked
+        // is true
+        if (eachRating.checked == true) {
+            rating = eachRating.value;
+            break;
+        }
+    }
+    console.log(name, email, rating);
 })
